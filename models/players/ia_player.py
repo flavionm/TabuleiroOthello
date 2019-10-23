82e8649a8a3d25aa46181677a55e02a16bd79b98
Flávio Sousa <fn_maia@outlook.com>
2019-10-22 23:52:41 -0300

    Finalizada implementação do algorítmo minimax (sem corte a-b)

--- models/players/ia_player.py
+++ models/players/ia_player.py
@@ -1,4 +1,7 @@
 class IA:
+    INFINITY = 2000000000
+    MAX_DEPTH = 4
+
     def __init__(self, color):
         if color == '@':
             self.color = color
@@ -7,40 +10,43 @@ class IA:
             self.color = color
             self.opp_color = '@'
 
-
     def play(self, board):
         return self.__minimax(board)
 
-
     def __minimax(self, board):
-        board, _ = self.__recursive_max(board, 1)
+        move, _ = self.__recursive_max(board, 1)
 
-        return board
-
+        return move
 
     def __recursive_max(self, board, depth):
-        curr_board = None
-        curr_score = -2000000000
-        for next_board in board.valid_moves(self.color):
-            _, next_score = self.__recursive_min(next_board, depth + 1)
+        if depth >= self.MAX_DEPTH:
+            return None, self.__h(board)
+
+        curr_move = None
+        curr_score = -self.INFINITY
+        for next_move in board.valid_moves(self.color):
+            board.play(next_move, self.color)
+            _, next_score = self.__recursive_min(board, depth + 1)
             if next_score > curr_score:
                 curr_score = next_score
-                curr_board = next_board
-        
-        return curr_board, curr_score
+                curr_move = next_move
 
+        return curr_move, curr_score
 
     def __recursive_min(self, board, depth):
-        curr_board = None
-        curr_score = 2000000000
-        for next_board in board.valid_moves(self.opp_color):
-            _, next_score = self.__recursive_max(next_board, depth + 1)
+        if depth >= self.MAX_DEPTH:
+            return board, self.__h(board)
+
+        curr_move = None
+        curr_score = self.INFINITY
+        for next_move in board.valid_moves(self.opp_color):
+            board.play(next_move, self.opp_color)
+            _, next_score = self.__recursive_max(board, depth + 1)
             if next_score < curr_score:
                 curr_score = next_score
-                curr_board = next_board
-        
-        return curr_board, curr_score
+                curr_move = next_move
 
+        return curr_move, curr_score
 
     def __h(self, board):
         return 0
